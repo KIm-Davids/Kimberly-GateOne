4041e60339a067645586e82236e19bcf23404247
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-05-08 13:25:32 +0100

    Random Question python

---
+++ GateOne/Kimberly-gateOne/Day 3/python/RandomQuestion.py
@@ -0,0 +1,143 @@
+randomNumberOne = 0
+randomNumberTwo = 0
+randomNumberTwenty = 0
+randomNumberThirty =0
+randomNumberThree = 0
+randomNumberFour = 0
+randomNumberFive = 0
+randomNumberEleven = 0
+randomNumberSix = 0 
+randomNumberSeven =  0
+randomNumberEight = 0 
+randomNumberNine = 0		
+randomNumberTen = 0
+
+
+randomNumberOne = ThreadLocalRandom.current().next(1,100)
+randomNumberTwo = ThreadLocalRandom.current().next(1,100)	
+randomNumberThree = ThreadLocalRandom.current().next(1,100)
+randomNumberFour = ThreadLocalRandom.current().next(1,100)
+randomNumberFive = ThreadLocalRandom.current().next(1,100)
+randomNumberEleven = ThreadLocalRandom.current().next(1,100)
+randomNumberSix = ThreadLocalRandom.current().next(1,100)
+randomNumberSeven = ThreadLocalRandom.current().next(1,100)
+randomNumberEight = ThreadLocalRandom.current().next(1,100)
+randomNumberNine = ThreadLocalRandom.current().next(1,100)		
+randomNumberTen = ThreadLocalRandom.current().next(1,100)	
+
+correcNumbers = 0
+ wrongNumbers = 0
+		
+
+int(input(("Welcome to Math Trivia")
+int(input(("Please try to answer all questions correctly")
+	
+
+for numbers in range(10)
+	int(input(("What is the maximum number between " + randomNumberSeven + " and "+ randomNumberTwo + "?"))
+	question1Result = Math.max(randomNumberSeven,randomNumberTwo)
+
+	if(question1 == question1Result):
+		print("It is correct")
+		correcNumbers++
+		
+	else:
+		print("It is wrong")
+		wrongNumbers++
+				
+
+ 
+	int(input(("Add the two numbers " + randomNumberThree + " and " + randomNumberSix))
+	 question2result = randomNumberThree + randomNumberSix
+		
+		if(question2 == question2result):
+			print("It is correct")
+			correcNumbers++
+		
+		else:
+			print("It is wrong")
+			wrongNumbers++
+				
+
+		
+			int(input("What is the minimum number between " + randomNumberFour + " and " + randomNumberOne))
+			
+			 question3result = Math.min(randomNumberFour, randomNumberEleven)
+	
+		if(question3 == question3result):
+			print("It is correct")
+			correcNumbers++
+		
+		else:
+			print("It is wrong")
+			wrongNumbers++
+				
+
+
+
+
+
+			int(input(("What is the square of " +randomNumberTen))
+			 
+			 question4result = (randomNumberTen * randomNumberTen)
+
+
+		if(question4 == question4result):
+			print("It is correct")
+			correcNumbers++
+		
+		else:
+			print("It is wrong")
+			wrongNumbers++
+				
+
+
+		
+			int(input(("What is the cube of " + randomNumberEight))
+			
+			 question5result = (randomNumberEight * randomNumberEight *randomNumberEight)
+
+		if(question5 == question5result):
+			print("It is correct")
+			correcNumbers++
+		
+		else:
+			print("It is wrong")
+			wrongNumbers++
+				
+
+
+
+
+
+			print("Your corret score are " + correcNumbers)
+			print("Your incorrect score are " + wrongNumbers)
+		
+			print(" ")
+
+			print("You have a another chance\nLet's play again")
+
+
+
+
+		
+
+
+
+		
+	
+
+		
+		
+
+
+				
+
+	
+
+		
+
+
+		
+
+
