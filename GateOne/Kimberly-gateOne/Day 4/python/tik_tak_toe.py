3a14e7e49cf064c2ab2c13b058dd1d8b9ae132a9
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-05-09 14:07:36 +0100

    Day 4

---
+++ GateOne/Kimberly-gateOne/Day 4/python/tik_tak_toe.py
@@ -0,0 +1,192 @@
+print("WELCOME TO MINI TIK TAK TOE GAME\n")
+
+character_picked = " "
+list_for_x = []
+list_for_o = []
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	position = input("Choose a position between '1','2','3','4','5','6','7,'8,','9'")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+	if(len(character_picked) > 0):
+		continue	
+
+
+if(list[0] =="x"):	
+	print("\n")
+	print("X")
+	print("\n")
+
+if(list[0] =="o"):	
+	print("\n")
+	print("O")
+	print("\n")
+
+character_picked1 = input("Let's continue !\n")
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+
+if(list[1] =="x"):	
+	print("\n")
+	print("\tX")
+	print("\n")
+
+if(list[1] =="o"):	
+	print("\n")
+	print("\tO")
+	print("\n")
+
+character_picked2 = input("Let's continue !\n")
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+
+if(list[2] =="x"):	
+	print("\n")
+	print("\t\tX")
+	print("\n")
+
+if(list[2] =="o"):	
+	print("\n")
+	print("\t\tO")
+	print("\n")
+
+character_picked3 = input("Let's continue !\n")
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+if(list[3] =="x"):	
+	print("\n")
+	print("X")
+	print("\n")
+
+if(list[3] =="o"):	
+	print("\n")
+	print("O")
+	print("\n")
+
+character_picked4 = input("Let's continue !\n")
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+
+if(list[4] =="x"):	
+	print("\n")
+	print("\tX")
+	print("\n")
+
+if(list[4] =="o"):	
+	print("\n")
+	print("\tO")
+	print("\n")
+
+character_picked5 = input("Let's continue !\n")
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+
+if(list[5] =="x"):	
+	print("\n")
+	print("\t\tX")
+	print("\n")
+
+if(list[5] =="o"):	
+	print("\n")
+	print("\t\tO")
+	print("\n")
+
+character_picked6 = input("Let's continue !\n")
+
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+if(list[6] =="x"):	
+	print("\n")
+	print("X")
+	print("\n")
+
+if(list[6] =="o"):	
+	print("\n")
+	print("O")
+	print("\n")
+
+character_picked7 = input("Let's continue !\n")
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+
+if(list[7] =="x"):	
+	print("\n")
+	print("\tX")
+	print("\n")
+
+if(list[7] =="o"):	
+	print("\n")
+	print("\tO")
+	print("\n")
+
+character_picked8 = input("Let's continue !\n")
+
+for numbers in range(0,9):
+	character_picked = input("Please enter an 'x' or 'o' to begin:\n")
+	if(character_picked == "x" or character_picked == "o"):
+		list.append(character_picked)
+		break
+
+
+if(list[8] =="x"):	
+	print("\n")
+	print("\t\t\tX")
+	print("\n")
+
+if(list[8] =="o"):	
+	print("\n")
+	print("\t\t\tO")
+	print("\n")
+
+character_picked9 = input("Let's continue !\n")
+
+
+print(list)
+
+
+
+
+
+	
+	
+	
+
+
+
