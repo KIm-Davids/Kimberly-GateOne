8d41063c85b40e47f991723cbfc64525aa033182
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-05-13 16:50:51 +0100

    Update

--- GateOne/Kimberly-gateOne/Day 7/Java/AtmMachineApp.java
+++ GateOne/Kimberly-gateOne/Day 7/Java/AtmMachineApp.java
@@ -1,83 +1,147 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class AtmMachineApp {
+		static ArrayList<String> welcomPage = new ArrayList<String>();
+		static ArrayList<String> userDetails = new ArrayList<String>();
+		static ArrayList<String> pin = new ArrayList<String>();
+		static ArrayList<String> deposit = new ArrayList<String>();
+
 		static Scanner input = new Scanner(System.in);
-		public static ArrayList<String> loginPage(){
 
-		ArrayList<String> userDetails = new ArrayList<String>();		
+			private static ArrayList<String> deposit(){
 
-			System.out.println("Please enter your name");
-			String userName = input.next();
-			userDetails.add(userName);
+				System.out.print("Enter amount to deposit");
+				String depositMoney = input.next();
+	
+				deposit.add(depositMoney);
 
 				System.out.println();
 				System.out.println();
+			
+				String pinNumber = 
+	
+				System.out.print("Please enter your pin");
+				int pin = input.nextInt();
 
-			System.out.print("Please enter your phone number");
-			String phoneNumber = input.next();
-			userDetails.add(phoneNumber);
+				return deposit;
+			}
 
+
+			private static int errorMessage(){
+
+				System.out.print(" Press 1 to try again or 2 to quit");
+				int errorInput = input.nextInt();			
+
+				if(errorInput == 1){
+					welcomeUserPage();
+				}
+					return errorInput;
 				
-				System.out.println();
-				System.out.println();
+			}	
 
-			System.out.println("Please enter your e-mail");
-			String email = input.next();
-			userDetails.add(email);
+
+
+			private static ArrayList<String> welcomeUserPage(){
+				
+				ArrayList<String> welcomeUserPage = new ArrayList<String>();
+
+				System.out.print("WELCOME TO KIMBERLY MULTINATIONAL BANK");
+	
+					System.out.println();
+					System.out.println();
+		
+				System.out.print("Lets get you started !");
+
+					System.out.println();
+					System.out.println();
+	
+				System.out.println("Please enter your First name");
+				String firstName = input.next();
+	
+				userDetails.add(firstName);
+
+				System.out.print("Please enter your Last name\n");
+				String lastName = input.next();
+
+				userDetails.add(lastName);
+
+				System.out.println("Create a pin");
+				String pinNumber = input.next();
+				
+				pin.add(pinNumber);	
+				
+		
+				return welcomeUserPage;
 
 			
+			
+			}
+
+
+			public static ArrayList<String> mainMenu(){
+
+				ArrayList<String> mainMenu = new ArrayList <String>();				
+
 				System.out.println();
 				System.out.println();
+			
+				System.out.print("Loading >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+				
+				System.out.println();
+				System.out.println();	
 
-			System.out.println("Please enter your address");
-			String address = input.next();
+				System.out.print("""
 
-			userDetails.add(address);
+				1 -> Deposit
+				
+				2 -> Withdraw Money
 
-			return userDetails;
-
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		public static void main(String[] args){
-	//	Scanner input = new Scanner(System.in);
-
-		System.out.println();
-		System.out.println();
-
+				3 -> Account Balance
 	
-		System.out.print("WELCOME TO KIM MULTINATIONAL BANK");
-	
-		System.out.println();
+				4 -> Tranfer Money
 
-		System.out.print("""
-
-		1 -> Create Account
-		2 -> Sign In
-
-			""");
+				5 -> Change pin 
 		
-		int loginPage = input.nextInt();
+				6 -> Close Account
 
-		switch(loginPage){
-						case 1: loginPage();
-						case 2: 
+					""");
+		
+				int loginPage = input.nextInt();
+
+				switch(loginPage){
+							case 1: deposit();
+								    break;
+							case 2: 
+							case 3:
+							case 4:
+							case 5:
+							case 6:
+							case 7: System.out.print("Thank you for your Patronage");
+							default: errorMessage();
+	
+
+						
+
+				}
+
+					return mainMenu;
+
+			}
+
+
 	
 
 
 
-		}
+
+
+
+
+
+	
+		public static void main(String[] args){
+		welcomeUserPage();
+		mainMenu();
+	
 	}                                                                                                                      				
 }
