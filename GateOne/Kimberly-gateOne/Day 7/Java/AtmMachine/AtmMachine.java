f2ab48f2786e954431f3f18d73e5842214aa0ee2
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-05-14 13:30:48 +0100

    Update

--- GateOne/Kimberly-gateOne/Day 7/Java/AtmMachine/AtmMachine.java
+++ GateOne/Kimberly-gateOne/Day 7/Java/AtmMachine/AtmMachine.java
@@ -1,11 +1,21 @@
 public class AtmMachine {
 	private String name;
+	private int pin;
 
 	public void setName(String name){
 		this.name = name;
+		
 	}
 
 	public String getName(){
 		return name;
+	}
+		
+	public void setPin(int pin){
+		this.pin = pin;
+	}
+
+	public int getPin(){
+		return pin;
 	}
 }
--- GateOne/Kimberly-gateOne/Day 7/Java/AtmMachine/AtmMachineAppRetry.java
+++ GateOne/Kimberly-gateOne/Day 7/Java/AtmMachine/AtmMachineAppRetry.java
@@ -5,16 +5,42 @@ public class AtmMachineAppRetry {
 		Scanner input = new Scanner(System.in);
 
 		AtmMachine myAccount = new AtmMachine();
+					
+		System.out.print("WELCOME TO BANKE MULTINATIONAL BANK");
+	
+			System.out.println();
+			System.out.println();
+		
+		System.out.print("Lets get you started !");
 
-		System.out.printf("Initial name is: %ns%n%n", myAccount.getName());
+			System.out.println();
+			System.out.println();
+	
+		System.out.println("Please enter your full name");
+		String userName = input.nextLine();
 
-		System.out.println("Please enter the name");
+			System.out.println();
+			System.out.println();
 
-		String theName = input.next();
-		myAccount.setName(theName);
-		System.out.printl();
+		System.out.println("Create a pin");
+		int pinNumber = input.nextInt();
+
+		System.out.print("Welcome User: 8366787112");
+	
+			System.out.println();
+			System.out.println();
+
+		
+		
+								
+		
+		myAccount.setName(userName);
+		System.out.println();
+
+		myAccount.setPin(pinNumber);
 
 		System.out.printf("Name in object myAccount is %n%s%n", myAccount.getName());
+		System.out.printf("Your pin is %n%d%n", myAccount.getPin());
 	
 		
 		
